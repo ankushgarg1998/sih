d51238f231e123a6802af46a81ee51aedb14047b
ankushgarg1998 <1998anky@gmail.com>
2019-07-10 11:02:06 +0530

    Jun10

--- client/scripts/firebase.js
+++ client/scripts/firebase.js
@@ -16,3 +16,60 @@ ref.on("value", function(snapshot) {
 }, function (error) {
     console.log("Error: " + error.code);
 });
+
+var ele = $('.first-chart');
+console.log(ele);
+
+
+var ctx = document.getElementsByClassName('first-chart')[0].getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes in India',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        },
+        {
+            label: '# of Votes in Pakistan',
+            data: [15, 55, 7, 12, 3, 1],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        elements: {
+            line: {
+                tension: 0.5
+            },
+            point: {
+                radius: 0
+              }
+        },
+        scales: {
+            xAxes: [{
+                gridLines: false
+                // ticks: {
+                //   callback: function (tick, index) {
+                //     // Jump every 7 values on the X axis labels to avoid clutter.
+                //     return index % 7 !== 0 ? '' : tick;
+                //   }
+                
+              }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: false
+          },
+    }
+});
