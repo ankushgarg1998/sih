dd6dbe23793f33beb19b4a7fe2923a858d196ec7
EktaSangwan <sangwanekta24@gmail.com>
2019-07-10 21:29:07 +0530

    minor changes

--- client/scripts/firebase.js
+++ client/scripts/firebase.js
@@ -73,3 +73,57 @@ var myChart = new Chart(ctx, {
           },
     }
 });
+
+
+var ct1 = document.getElementsByClassName('second-chart')[0].getContext('2d');
+var myChart1 = new Chart(ct1, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Air Pollution duw to SO2',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Air Pollution due to CO',
+            data: [15, 55, 7, 12, 3, 1],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        elements: {
+            line: {
+                tension: 0.5
+            },
+            point: {
+                radius: 0
+              }
+        },
+        scales: {
+            xAxes: [{
+                gridLines: false
+                // ticks: {
+                //   callback: function (tick, index) {
+                //     // Jump every 7 values on the X axis labels to avoid clutter.
+                //     return index % 7 !== 0 ? '' : tick;
+                //   }
+                
+              }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: false
+          },
+    }
+});
